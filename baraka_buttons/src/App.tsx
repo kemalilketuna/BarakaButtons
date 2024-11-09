90d9d1647f8466828741377d2dcd03216426c4f4
kemal ilke tuna <kemalilketuna@gmail.com>
2024-11-09 20:58:51 +0300

    updates

--- baraka_buttons/src/App.tsx
+++ baraka_buttons/src/App.tsx
@@ -1,14 +1,28 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { AddRoomBox } from "./components/RoomBoxs"
+import { Spacer } from "./components/Spacer"
+import { theme } from "./Theme"
+import { ThemeProvider } from "@mui/material/styles"
+import { CssBaseline } from "@mui/material"
+import MainIPField from "./components/MainIPField"
 
 const App = () => {
     return (
-        <>
-            <h1>Baraka Buttons</h1>
-            <Box>
-                <AddRoomBox />
-            </Box>
-        </>
+        <Box sx={{ padding: '5vw', paddingTop: '2vw', paddingBottom: '2vw', width: '100vw', height: '100vh' }}>
+            <ThemeProvider theme={theme}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+                    <Typography variant="h1">Baraka Buttons</Typography>
+                    <Box sx={{ paddingTop: '3vw' }}>
+                        <MainIPField />
+                    </Box>
+                </Box>
+                <Spacer height="2vw" />
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '2vw' }}>
+                    <AddRoomBox />
+                </Box>
+                <CssBaseline />
+            </ThemeProvider>
+        </Box >
     )
 }
 
--- baraka_buttons/src/components/RoomBoxs.tsx
+++ baraka_buttons/src/components/RoomBoxs.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react"
-import { Box } from "@mui/material"
+import { Box, Typography, TextField, TextFieldProps, Button } from "@mui/material"
 import AddIcon from '@mui/icons-material/Add';
 import { styled } from "@mui/material/styles";
 
 const StyledBox = styled(Box)({
-    width: '250px',
-    height: '250px',
+    width: '300px',
+    height: '300px',
     border: '1px solid #ccc',
     borderRadius: '10px',
     display: 'flex',
@@ -13,13 +13,93 @@ const StyledBox = styled(Box)({
     justifyContent: 'center',
 });
 
+const StyledClickableBox = styled(Box)({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '&:hover': {
+        backgroundColor: '#0a0a0a',
+    },
+    height: '100%',
+    width: '100%',
+    borderRadius: '10px',
+});
+
+const StyledFormBox = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: '100%',
+    width: '100%',
+    paddingBottom: '1rem',
+    paddingTop: '1rem',
+});
+
+const StyledTextField = (props: TextFieldProps) => {
+    return <TextField {...props} sx={{
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'primary.main', // Set border color to primary
+            },
+            '&:hover fieldset': {
+                borderColor: 'primary.main', // Set border color on hover
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: 'primary.main', // Set border color when focused
+            },
+        },
+    }} />
+}
+
+
 const AddRoomBox = () => {
+    const [isClicked, setIsClicked] = useState(false);
+    const [roomName, setRoomName] = useState('');
+    const [roomIP, setRoomIP] = useState('');
+
+    const toggleClicked = () => {
+        setIsClicked(!isClicked);
+    }
+
+
+    const createRoom = () => {
+        if (roomName === '' || roomIP === '') {
+            return;
+        }
+        setIsClicked(false);
+        console.log(roomName, roomIP);
+    }
+
     return (
         <StyledBox>
-            <AddIcon />
+            {!isClicked ? (
+                <StyledClickableBox onClick={toggleClicked}>
+                    <AddIcon style={{ fontSize: '140px' }} />
+                    <Typography variant="h4">Add Room</Typography>
+                </StyledClickableBox>
+            ) : (
+                <StyledFormBox>
+                    <Typography variant="h4">Create Room</Typography>
+                    <StyledTextField label="Enter room name" sx={{ width: '100%' }} value={roomName} onChange={(e) => setRoomName(e.target.value)} />
+                    <StyledTextField
+                        label="Enter room IP"
+                        sx={{ width: '100%' }}
+                        value={roomIP}
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            if (/^\d{0,3}$/.test(value)) {
+                                setRoomIP(value);
+                            }
+                        }}
+                    />
+                    <Button variant="contained" color="primary" onClick={createRoom}>Create Room</Button>
+                </StyledFormBox>
+            )}
         </StyledBox>
     )
 }
 
 
-export { AddRoomBox }
+export { AddRoomBox as AddRoomBox }
