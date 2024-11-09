606b4fb370c28f6b3b288b25e1d1d2b0faeb01d9
kemal ilke tuna <kemalilketuna@gmail.com>
2024-11-09 20:46:50 +0300

    Create Theme.tsx

---
+++ baraka_buttons/src/Theme.tsx
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#f5f5f5',
+        },
+        secondary: {
+            main: '#242424',
+            dark: '#0a0a0a',
+        },
+        background: {
+            default: '#242424',
+        },
+        text: {
+            primary: '#f5f5f5',
+            secondary: '#f5f5f5',
+        },
+    },
+});
