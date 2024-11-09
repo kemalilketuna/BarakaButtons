5ba0ab59785a27c660cac7b794e5fa29a0fccc5a
kemal ilke tuna <kemalilketuna@gmail.com>
2024-11-09 20:56:52 +0300

    Create MainIPField.tsx

---
+++ baraka_buttons/src/components/MainIPField.tsx
@@ -0,0 +1,29 @@
+import { TextField } from "@mui/material";
+import { useState } from "react";
+
+const MainIPField = () => {
+    const [mainIP, setMainIP] = useState(localStorage.getItem('mainIP') || '');
+
+    return <TextField label="Main IP" sx={{
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'primary.main', // Set border color to primary
+            },
+            '&:hover fieldset': {
+                borderColor: 'primary.main', // Set border color on hover
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: 'primary.main', // Set border color when focused
+            },
+        },
+    }} value={mainIP}
+        onChange={(e) => {
+            const value = e.target.value;
+            if (/^\d{0,3}$/.test(value)) {
+                localStorage.setItem('mainIP', value);
+                setMainIP(value);
+            }
+        }} />
+}
+
+export default MainIPField;
