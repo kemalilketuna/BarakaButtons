a3d377096a0af2391fc8473c6bde84b4a0662f66
kemal ilke tuna <kemalilketuna@gmail.com>
2024-11-10 12:20:21 +0300

    Update App.tsx

--- baraka_buttons/src/App.tsx
+++ baraka_buttons/src/App.tsx
@@ -8,6 +8,7 @@ import MainIPField from "./components/MainIPField"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import RoomBox from "./components/RoomBox"
+import Dashboard from "./components/Dashboard"
 
 interface Room {
     name: string;
@@ -19,20 +20,32 @@ const MainContent = () => {
 
     return (
         <Box sx={{ padding: '5vw', paddingTop: '2vw', paddingBottom: '2vw', width: '100vw', height: '100vh' }}>
+
+            {/* Title */}
             <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
                 <Typography fontSize="4vw">Baraka Buttons</Typography>
                 <Box sx={{ paddingTop: '2vw' }}>
                     <MainIPField />
                 </Box>
             </Box>
+
             <Spacer height="2vw" />
+
+            {/* Room Buttons */}
             <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '2vw' }}>
                 <AddRoomBox />
                 {rooms.map((room: Room) => (
                     <RoomBox room={room} />
                 ))}
             </Box>
-        </Box>
+
+            <Spacer height="2vw" />
+
+            {/* Dashboard */}
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Dashboard />
+            </Box>
+        </Box >
     );
 }
 
