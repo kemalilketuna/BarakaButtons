e7afdd48ff0bd9082a336be6eb0d03b4112c9939
kemal ilke tuna <kemalilketuna@gmail.com>
2024-11-10 12:20:19 +0300

    Create Dashboard.tsx

---
+++ baraka_buttons/src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+
+import { Box } from "@mui/material";
+
+const Dashboard = () => {
+    return (
+        <Box sx={{ height: '55vh', width: '80vw', borderRadius: '10px', border: '1px solid #fff' }}>
+
+        </Box>
+    );
+}
+
+export default Dashboard;
