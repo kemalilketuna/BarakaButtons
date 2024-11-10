5bea58f6080182f6493712ad615babf9dd93fd08
kemal ilke tuna <kemalilketuna@gmail.com>
2024-11-10 12:56:16 +0300

    Update Dashboard.tsx

--- baraka_buttons/src/components/Dashboard.tsx
+++ baraka_buttons/src/components/Dashboard.tsx
@@ -1,10 +1,10 @@
-
 import { Box } from "@mui/material";
+import AddRoomForm from "./AddRoomForm";
 
 const Dashboard = () => {
     return (
         <Box sx={{ height: '55vh', width: '80vw', borderRadius: '10px', border: '1px solid #fff' }}>
-
+            <AddRoomForm />
         </Box>
     );
 }
